033d7802d999236bf25f51cdf1e7634bd554829f
Sean Seung Won Lee <swdev1202@gmail.com>
2020-04-11 17:09:18 -0700

    debug eval

--- tools/kitti_object_eval_python/eval.py
+++ tools/kitti_object_eval_python/eval.py
@@ -461,6 +461,7 @@ def eval_class(gt_annos,
     Returns:
         dict of recall, precision and aos
     """
+    print(f'gt_annos len = {len(gt_annos), dt_annos_len = {len(dt_annos)}}')
     assert len(gt_annos) == len(dt_annos)
     num_examples = len(gt_annos)
     split_parts = get_split_parts(num_examples, num_parts)
